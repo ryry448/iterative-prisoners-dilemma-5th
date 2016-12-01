1dc60fc0fe3ad7a3ab948fcb330af8dd66fc2bd6
dannysuchta <240651@shakopeeschools.org>
2016-12-01 13:14:10 -0600

    Team 2 Final Algorithm

--- team2.py
+++ team2.py
@@ -17,6 +17,8 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    
+    
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -25,8 +27,12 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if len(my_history)==0: 
+        return 'b'
+    elif my_history[-1]=='b' and their_history[-1]=='b':
+        return 'b' 
+    else:
+        return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
