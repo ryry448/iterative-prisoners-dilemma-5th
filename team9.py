89411542d8d0f6e9ec23b94578562cc8ba92d3fe
Trumanvb <226875@c1mrxe0uh3qd.isd720.com>
2016-12-01 13:15:35 -0600

    team9
    everything done

--- team9.py
+++ team9.py
@@ -1,16 +1,17 @@
+
 ####
 # Each team's file must define four tokens:
-#     team_name: Team9
+#     team9
 #     strategy_name: a string
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Team9' # Only 10 chars displayed.
+team_name = 'The name the team gives to itself' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
-def move(my_history, their_history, my_score, their_score):
+def titfortat(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
@@ -26,9 +27,35 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
-
-    ###################
+    
+    
+    #tit for tat code but starts with betray, doesn't work yet
+    
+    if their_history is '':
+        return 'b'
+    if their_history is 'b':
+        return 'b'
+    else:
+        return 'c'
+    
+    
+    
+    #test1 betray first move
+   # if '' in my_history:
+    #    return 'b'
+    
+    #test 2 betray no matter what
+    #if their_history is 'b' or 'c':
+     #   return 'b'
+    # remove hashtag for test3 if 'b' in their_history:
+     #   return 'b'
+    #else:
+     #   return 'c'
+    
+    #always collude
+    #if their_history is 'b' or 'c':
+       # return 'c'
+    
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
@@ -53,16 +80,42 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
+         print 'First round betray passes'
+     
+     
+      # Note the scores are for testing move().
               # The history and scores don't need to match unless
               # that is relevant to the test of move(). Here,
               # the simulation (if working correctly) would have awarded 
               # 300 to me and -750 to them. This test will pass if and only if
               # move('bbb', 'ccc', 0, 0) returns 'b'.
+              
+     # Test 2: Continue betraying if they collude despite being betrayed.
+    if test_move(my_history='bbb',
+              their_history='ccc', 
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b'):
+            print 'Infinite betray passes'
+    
+    
+    #test 3: grudge(works)
+    if test_move(my_history='c', their_history='b',
+    my_score=-750,
+    their_score=300,
+    result='b'):
+        print 'Grudge passes'
+    #test 4: tit for tat
+    if test_move(my_history='c', their_history='b',
+    my_score=0, their_score=0, result='b'):
+        print 'Tit for tat betray passes'
+    #test 5: tit for tat collude
+    if test_move(my_history='bbc', their_history='bcb', my_score=0, their_score=0, result='c'):
+        print 'Tit for tat collude passes'
+        
+    #Test 6: grudge but should not retaliate since enemy uses collude
+    if test_move(my_history='c', their_history='c', my_score=0, their_score=0, result='c'):
+        print 'Grudge collude passes'
+    #test 7: always collude
+    if test_move(my_history='c', their_history='b', my_score=-500, their_score=150, result='c'):
+        print 'Always collude passes'         
