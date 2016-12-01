3bb1037906f1695c6c1e5efbc133c20c9b18e282
Trumanvb <226875@c1mrxe0uh3qd.isd720.com>
2016-12-01 13:13:16 -0600

    adde

--- team9.py
+++ team9.py
@@ -28,7 +28,7 @@ def move(my_history, their_history, my_score, their_score):
     
     return 'c'
 
-    
+    ###################
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
