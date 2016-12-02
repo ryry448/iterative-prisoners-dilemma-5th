c534dc2683b834b936972c6d06c7946baec87bf2
Trumanvb <226875@c1mrxe0uh3qd.isd720.com>
2016-12-02 12:49:00 -0600

    team9
    changed desc

--- team9.py
+++ team9.py
@@ -7,9 +7,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'team9' # Only 10 chars displayed.
+strategy_name = 'titfortat'
+strategy_description = 'Replicate the enemies'
     
 def titfortat(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
