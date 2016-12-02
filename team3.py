c469321f8a8f34ac63d9de34dc06af56d2246462
Markus Babris <224473@shakopeeschools.org>
2016-12-02 12:39:53 -0600

    Update team3 file algorithm

--- team3.py
+++ team3.py
@@ -1,32 +1,30 @@
 ####
 # Each team's file must define four tokens:
-#     team_name: Soapy & Shardo
+#     team_name: Joe Biden
 #     strategy_name: a string
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Soapy & Shardo' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+import random
+
+team_name = 'Joe Biden' # Only 10 chars displayed.
+strategy_name = 'Meme Magic'
+strategy_description = 'Decisions are entirely random.'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+ 
+    choice = random.randint(1, 10)
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+    if choice % 2 == 0:
+        choice = random.randint(1, 100)
+        if choice % 2 == 0: 
+            return 'c'
+        else:
+            return 'b'
+    if choice % 2 != 0:
+        return 'b'
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -65,4 +63,4 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')                      
